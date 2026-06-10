48c4ba49360319b6962841b9a5cb83c292642c1d
Aluno <e023aluno@outlook.com>
2026-06-10 17:43:45 -0300

    aplicacao funcionando, back e front

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
 import express from 'express';
+import cors from 'cors'; 
 import produtoRoutes from './routes/produtoRoutes';
 import { pool } from './config/database';
 import path from 'path';
+
 const PORT = 3000;
 const app = express();
+
 app.use(express.json());
+app.use(cors()); // <-- 2. ADICIONE ESSA LINHA (Sempre antes das rotas)
+
 app.use(express.static(path.join(__dirname, 'views')));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'view', 'index.html'));
 }); 
+
 app.use('/api', produtoRoutes);
+
 pool.getConnection()
     .then(() => { 
         console.log('Conectado ao MYSQL'); 
@@ -19,4 +27,4 @@ pool.getConnection()
     }) 
     .catch((error: any) => {
         console.error('Erro ao conectar ao MYSQL', error);
-    }); 
+    });
